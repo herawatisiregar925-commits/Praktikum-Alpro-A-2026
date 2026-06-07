6b754c1a762fd3302939f7d93c9d1138f91ef598
Your Name <you@example.com>
2026-06-07 15:33:58 +0700

    Quiz Alpro 1

---
+++ Quiz Alpro/soal1.py
@@ -0,0 +1,13 @@
+Menu = [["Nasi Goreng", 15000], ["Es Teh", 5000], ["Mie Goreng", 15000], ["Mie Rebus", 10000], ["Es Jeruk", 5000]]
+for i in range(len(Menu)):
+    print(f"{i+1} {Menu[i][0]} harga = {Menu[i][1]}")
+
+nomorMenu =(1, 2, 3, 4, 5)
+
+i = 0
+pilihan = int(input("Masukkan nomor menu:"))
+if pilihan in nomorMenu :
+    print(f"{"pilihan"} {Menu[pilihan -1][0]}) harga = {Menu[pilihan -1][1]}")
+else:
+    pilihan >= 5
+    print("ERROR" " " "nomor menu tidak valid")
